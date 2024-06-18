13caf7f817956d78849731b15d5c3b73a9a8d19b
Patrolin <patrik@balas.cz>
2024-06-18 21:10:43 +0200

    keys and state

--- jsgui.js
+++ jsgui.js
@@ -1,22 +1,31 @@
 // base component
 class Component {
-  constructor(createElement, styles, attributes, key) {
-    this.styles = styles; // Record<string, string>
-    this.attributes = attributes; // Record<string, string>
+  constructor(_render, styles, attributes, key) {
+    this.styles = styles ?? {}; // Record<string, string>
+    this.attributes = attributes ?? {}; // Record<string, string>
     this.children = []; // Component[]
-    this.keyedChildCount = 0; // number
-    this.key = key; // string
+    this.key = key != null ? String(key) : null; // string
+    this.usedKeys = new Set(); // Set<string>
+    this.indexedChildCount = 0; // number
     this.index = 0; // number
-    this.createElement = createElement; // function () {}
+    this._render = _render; // function () {}
     this.metadata = null; // ComponentMetadata
   }
   append(child) {
     if (child.key) {
-      this.keyedChildCount++;
-      child.metadata = this.metadata.keyToChild[child.key] ?? new ComponentMetadata();
+      if (this.usedKeys.has(child.key)) {
+        console.warn(`Duplicate key: '${child.key}'`);
+      }
+      this.usedKeys.add(child.key);
+      const newMetadata = this.metadata.keyToChild[child.key] ?? new ComponentMetadata();
+      this.metadata.keyToChild[child.key] = newMetadata;
+      child.metadata = newMetadata;
     } else {
-      child.index = this.children.length - this.keyedChildCount;
-      child.metadata = this.metadata.indexToChild[child.index] ?? new ComponentMetadata();
+      child.index = this.indexedChildCount++;
+      if (child.index >= this.metadata.indexToChild.length) {
+        this.metadata.indexToChild.push(new ComponentMetadata());
+      }
+      child.metadata = this.metadata.indexToChild[child.index];
     }
     child.metadata.parentComponent = this.metadata;
     this.children.push(child);
@@ -34,77 +43,91 @@ class ComponentMetadata {
   constructor() {
     this.parent = null; // ComponentMetadata
     this.indexToChild = []; // ComponentMetadata[]
+    this.prevIndexedChildCount = null; // number | null
     this.keyToChild = {} // Record<string, ComponentMetadata>
     this.state = null; // any
     this.prevState = null; // any
-    this.node = null; // HTMLElement | null;
+    this.prevNode = null; // HTMLElement | null;
   }
 }
 
 // render
 let _rootMetadata = null; // ComponentState | null;
-let _componentToRerender = null; // Component | null;
-let _metadataToRerender = null // ComponentMetadata | null;
 function renderRoot(rootComponent) {
   _rootMetadata = new ComponentMetadata();
   rootComponent.metadata = _rootMetadata;
-  window.onload = () => _createElements(document.body, rootComponent);
+  window.onload = () => _renderElements(document.body, rootComponent);
 }
-function _createElements(parentNode, component) {
-  // TODO: add checks for index/key
-  const node = component.createElement.bind(component)();
-  if (node instanceof Element) {
-    _applyStyles(node, component.styles, component.attributes);
-    const prevNode = component.metadata.node;
-    if (prevNode) {
-      prevNode.after(node);
-      prevNode.remove();
-    } else if (parentNode) {
-      parentNode.append(node)
+function _renderElements(parentNode, component) {
+  // render elements
+  const node = component._render.bind(component)();
+  // check if indices changed
+  const indexedChildCount = component.indexedChildCount;
+  const prevIndexedChildCount = component.metadata.prevIndexedChildCount;
+  if (prevIndexedChildCount === null) {
+    component.metadata.prevIndexedChildCount = indexedChildCount;
+  } else if (indexedChildCount !== prevIndexedChildCount) {
+    console.error(`indexedChildCount changed ${prevIndexedChildCount} -> ${indexedChildCount} at`, component._render)
+    throw `Varying children should have a "key" prop.`;
+  }
+  // filter keyed items
+  const usedKeys = component.usedKeys;
+  component.keyToChild = Object.fromEntries(Object.entries(component.metadata.keyToChild).filter(([k, _v]) => usedKeys.has(k)));
+  component.metadata.prevState = {...component.metadata.state};
+  // append elements
+  if (node) {
+    for (let [k, v] of Object.entries(component.styles)) {
+      node.style[k] = v;
     }
-    component.metadata.node = node;
+    for (let [k, v] of Object.entries(component.attributes)) {
+      node.setAttribute(k, v);
+    }
+    const prevNode = component.metadata.prevNode;
+    if (prevNode) {
+      prevNode.remove();
+    }
+    parentNode.append(node)
+    component.metadata.prevNode = node;
     parentNode = node;
   }
   for (let child of component.children) {
-    _createElements(parentNode, child);
-  }
-}
-function _applyStyles(e, styles={}, attributes={}) {
-  for (let [k, v] of Object.entries(styles)) {
-    e.style[k] = v;
-  }
-  for (let [k, v] of Object.entries(attributes)) {
-    e.setAttribute(k, v);
+    _renderElements(parentNode, child);
   }
 }
 
-// rerender
-let _willRenderNextFrame = false;
-function _rerender(parentNode) {
-  _createElements(parentNode, _componentToRerender);
-  _willRenderNextFrame = false;
+// rerender // TODO: merge rerenders?
+let _componentToRerender = null; // Component | null;
+let _metadataToRerender = null // ComponentMetadata | null;
+let _rerenderNextFrame = false;
+function _rerender(_parentNode) {
+  _componentToRerender.usedKeys = new Set();
+  _componentToRerender.indexedChildCount = 0;
+  _componentToRerender.children = [];
+  const parentNode = _parentNode ?? (_componentToRerender.metadata === _rootMetadata ? document.body : null);
+  _renderElements(parentNode, _componentToRerender);
+  _rerenderNextFrame = false;
   _componentToRerender = null;
   _metadataToRerender = null;
 }
 function _rerenderNextFrame(component) {
-  if (!_willRenderNextFrame) {
+  if (!_rerenderNextFrame) {
     requestAnimationFrame(() => _rerender());
-    _willRenderNextFrame = true;
+    _rerenderNextFrame = true;
   }
   if (_isValidAndHigherThanComponentToRerender(component.metadata)) {
     _componentToRerender = component;
   }
 }
 function _isValidAndHigherThanComponentToRerender(metadata) {
-  if (!metadata.parent.indexToChild.includes(metadata) && !(metadata.key in metadata.parent.keyToChild))
-    return false;
-  let v;
-  for (v = metadata; v.parent != null; v = v.parent) {
-    if (v === _metadataToRerender) {
-      return false;
+  let curr = metadata;
+  if (metadata.parent && (metadata.parent.indexToChild.includes(metadata) || (metadata.key in metadata.parent.keyToChild))) {
+    for (; curr.parent != null; curr = curr.parent) {
+      if (curr === _metadataToRerender) {
+        return false;
+      }
     }
   }
-  return v === _rootMetadata;
+  return curr === _rootMetadata;
 }
 
 // components
@@ -120,3 +143,4 @@ function div(styles, attributes, key) {
     return document.createElement('div');
   }, styles, attributes, key);
 }
+// TODO: input components
