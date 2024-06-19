f7984b8d3a1a981740f495c189082192e74fb35f
Patrolin <patrik@balas.cz>
2024-06-19 16:49:29 +0200

    c

--- jsgui.js
+++ jsgui.js
@@ -1,50 +1,63 @@
 // base component
 class Component {
-  constructor(_render, styles, attributes, key) {
-    this.styles = styles ?? {}; // Record<string, string>
-    this.attributes = attributes ?? {}; // Record<string, string>
+  constructor(_render, args = [], props = {}) {
+    this.name = _render.name; // string
+    this.args = args // any[]
+    this.props = props; // Record<string, any>
     this.children = []; // Component[]
-    this.key = key != null ? String(key) : null; // string
-    this.usedKeys = new Set(); // Set<string>
-    this.indexedChildCount = 0; // number
-    this.index = 0; // number
-    this._render = _render; // function () {}
-    this.metadata = null; // ComponentMetadata
+    this._render = _render; // function (...) {}
+    this._ = null; // ComponentMetadata
   }
   append(child) {
-    if (child.key) {
-      if (this.usedKeys.has(child.key)) {
-        console.warn(`Duplicate key: '${child.key}'`);
+    const {_} = this;
+    const key = child.props.key;
+    if (key) {
+      if (_.usedKeys.has(key)) {
+        console.warn(`Duplicate key: '${key}'`);
       }
-      this.usedKeys.add(child.key);
-      const newMetadata = this.metadata.keyToChild[child.key] ?? new ComponentMetadata();
-      this.metadata.keyToChild[child.key] = newMetadata;
-      child.metadata = newMetadata;
+      _.usedKeys.add(key);
+      const newMetadata = _.keyToChild[key] ?? new ComponentMetadata();
+      _.keyToChild[key] = newMetadata;
+      child._ = newMetadata;
     } else {
-      child.index = this.indexedChildCount++;
-      if (child.index >= this.metadata.indexToChild.length) {
-        this.metadata.indexToChild.push(new ComponentMetadata());
+      const childIndex = _.currIndexedChildCount++;
+      if (childIndex >= _.indexToChild.length) {
+        _.indexToChild.push(new ComponentMetadata());
       }
-      child.metadata = this.metadata.indexToChild[child.index];
+      child._ = _.indexToChild[childIndex];
+      child._.index = childIndex;
     }
-    child.metadata.parentComponent = this.metadata;
+    child._.parent = this._;
     this.children.push(child);
     return child;
   }
   useState(defaultState) {
-    if (this.metadata.state === null) this.metadata.state = defaultState;
-    return this.metadata.state;
+    if (this._.state === null) this._.state = defaultState;
+    return this._.state;
   }
   rerender() {
     _rerenderNextFrame(this);
   }
 }
+function makeComponent(_render) {
+  return (...argsOrProps) => {
+    const args = argsOrProps.slice(0, _render.length);
+    const props = argsOrProps[_render.length];
+    return new Component(_render, args, props);
+  }
+}
 class ComponentMetadata {
   constructor() {
+    // navigation
     this.parent = null; // ComponentMetadata
     this.indexToChild = []; // ComponentMetadata[]
-    this.prevIndexedChildCount = null; // number | null
     this.keyToChild = {} // Record<string, ComponentMetadata>
+    // metadata
+    this.currIndexedChildCount = 0; // number
+    this.prevIndexedChildCount = null; // number | null
+    this.usedKeys = new Set(); // Set<string>
+    // state
+    this.index = 0; // number
     this.state = null; // any
     this.prevState = null; // any
     this.prevNode = null; // HTMLElement | null;
@@ -55,39 +68,43 @@ class ComponentMetadata {
 let _rootMetadata = null; // ComponentState | null;
 function renderRoot(rootComponent) {
   _rootMetadata = new ComponentMetadata();
-  rootComponent.metadata = _rootMetadata;
+  rootComponent._ = _rootMetadata;
   window.onload = () => _renderElements(document.body, rootComponent);
 }
 function _renderElements(parentNode, component) {
   // render elements
-  const node = component._render.bind(component)();
+  const {_} = component;
+  _.usedKeys = new Set(); // Set<string>
+  _.currIndexedChildCount = 0; // number
+  const node = component._render.bind(component)(...component.args, component.props);
   // check if indices changed
-  const indexedChildCount = component.indexedChildCount;
-  const prevIndexedChildCount = component.metadata.prevIndexedChildCount;
+  const currIndexedChildCount = _.currIndexedChildCount;
+  const prevIndexedChildCount = _.prevIndexedChildCount;
   if (prevIndexedChildCount === null) {
-    component.metadata.prevIndexedChildCount = indexedChildCount;
-  } else if (indexedChildCount !== prevIndexedChildCount) {
-    console.error(`indexedChildCount changed ${prevIndexedChildCount} -> ${indexedChildCount} at`, component._render)
+    _.prevIndexedChildCount = currIndexedChildCount;
+  } else if (currIndexedChildCount !== prevIndexedChildCount) {
+    console.error(`indexedChildCount changed ${prevIndexedChildCount} -> ${currIndexedChildCount} at`, component._render)
     throw `Varying children should have a "key" prop.`;
   }
   // filter keyed items
   const usedKeys = component.usedKeys;
-  component.keyToChild = Object.fromEntries(Object.entries(component.metadata.keyToChild).filter(([k, _v]) => usedKeys.has(k)));
-  component.metadata.prevState = {...component.metadata.state};
+  _.keyToChild = Object.fromEntries(Object.entries(_.keyToChild).filter(([k, _v]) => usedKeys.has(k)));
+  _.prevState = {..._.state};
   // append elements
+  const {style = {}, attribute = {}} = component.props;
   if (node) {
-    for (let [k, v] of Object.entries(component.styles)) {
+    for (let [k, v] of Object.entries(style)) {
       node.style[k] = v;
     }
-    for (let [k, v] of Object.entries(component.attributes)) {
+    for (let [k, v] of Object.entries(attribute)) {
       node.setAttribute(k, v);
     }
-    const prevNode = component.metadata.prevNode;
+    const prevNode = _.prevNode;
     if (prevNode) {
       prevNode.remove();
     }
     parentNode.append(node)
-    component.metadata.prevNode = node;
+    _.prevNode = node;
     parentNode = node;
   }
   for (let child of component.children) {
@@ -98,29 +115,28 @@ function _renderElements(parentNode, component) {
 // rerender // TODO: merge rerenders?
 let _componentToRerender = null; // Component | null;
 let _metadataToRerender = null // ComponentMetadata | null;
-let _rerenderNextFrame = false;
+let _doRerenderNextFrame = false;
 function _rerender(_parentNode) {
-  _componentToRerender.usedKeys = new Set();
-  _componentToRerender.indexedChildCount = 0;
   _componentToRerender.children = [];
-  const parentNode = _parentNode ?? (_componentToRerender.metadata === _rootMetadata ? document.body : null);
+  const parentNode = _parentNode ?? (_componentToRerender._ === _rootMetadata ? document.body : null);
   _renderElements(parentNode, _componentToRerender);
-  _rerenderNextFrame = false;
+  _doRerenderNextFrame = false;
   _componentToRerender = null;
   _metadataToRerender = null;
 }
 function _rerenderNextFrame(component) {
-  if (!_rerenderNextFrame) {
+  if (!_doRerenderNextFrame) {
     requestAnimationFrame(() => _rerender());
-    _rerenderNextFrame = true;
+    _doRerenderNextFrame = true;
   }
-  if (_isValidAndHigherThanComponentToRerender(component.metadata)) {
+  if (_isValidAndHigherThanComponentToRerender(component._)) {
     _componentToRerender = component;
   }
 }
-function _isValidAndHigherThanComponentToRerender(metadata) {
-  let curr = metadata;
-  if (metadata.parent && (metadata.parent.indexToChild.includes(metadata) || (metadata.key in metadata.parent.keyToChild))) {
+function _isValidAndHigherThanComponentToRerender(_) {
+  let curr = _;
+  // TODO: _.key doesn't exist
+  if (_.parent && (_.parent.indexToChild.includes(_) || (_.key in _.parent.keyToChild))) {
     for (; curr.parent != null; curr = curr.parent) {
       if (curr === _metadataToRerender) {
         return false;
@@ -130,17 +146,13 @@ function _isValidAndHigherThanComponentToRerender(metadata) {
   return curr === _rootMetadata;
 }
 
-// components // TODO: do away with new Component()?
-function span(text, styles, attributes, key) {
-  return new Component(() => {
-    const e = document.createElement('span');
-    e.innerText = text;
-    return e;
-  }, styles, attributes, key);
-}
-function div(styles, attributes, key) {
-  return new Component(() => {
-    return document.createElement('div');
-  }, styles, attributes, key);
-}
+// components
+const span = makeComponent(function span(text, _props) {
+  const e = document.createElement('span');
+  e.innerText = text;
+  return e;
+});
+const div = makeComponent(function div() {
+  return document.createElement('div');
+})
 // TODO: input components
