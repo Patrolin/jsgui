580c0fa5e62c81c4e6177d7bf63d6fbfd2305b11
Patrolin <patrik@balas.cz>
2024-06-22 15:19:34 +0200

    fix rerender, support this.useMedia()

--- jsgui.js
+++ jsgui.js
@@ -1,38 +1,53 @@
 // base component
 class Component {
-  constructor(_render, args = [], props = {}) {
+  constructor(_render, args = [], key = null, props = {}) {
     this.name = _render.name; // string
     this.args = args // any[]
+    this.key = (key != null) ? String(key) : null;
     this.props = props; // Record<string, any>
     this.children = []; // Component[]
     this._render = _render; // function (...) {}
     this._ = null; // ComponentMetadata | RootComponentMetadata
+    this._indexedChildCount = 0; // number
+    this._usedKeys = new Set(); // Set<string>
   }
   append(child) {
-    const {_} = this;
-    const key = child.props.key;
-    if (key) {
-      if (_.usedKeys.has(key)) {
-        console.warn(`Duplicate key: '${key}'`);
-      }
-      _.usedKeys.add(key);
-      const newMetadata = _.keyToChild[key] ?? new ComponentMetadata();
-      _.keyToChild[key] = newMetadata;
-      child._ = newMetadata;
-    } else {
-      const childIndex = _.currIndexedChildCount++;
-      if (childIndex >= _.indexToChild.length) {
-        _.indexToChild.push(new ComponentMetadata());
-      }
-      child._ = _.indexToChild[childIndex];
-      child._.index = childIndex;
+    let {_, _usedKeys} = this;
+    let key = child.key;
+    if (key === null) {
+      key = `${child.name}-${this._indexedChildCount++}`;
     }
+    if (_usedKeys.has(key)) console.warn(`Duplicate key: '${key}'`, this);
+    _usedKeys.add(key);
+    const child_ = _.keyToChild[key] ?? new ComponentMetadata();
+    _.keyToChild[key] = child_;
+    child._ = child_;
+    child_.root = _.root;
+    child_.parent = _;
+    child_.gcFlag = _.gcFlag;
     this.children.push(child);
     return child;
   }
   useState(defaultState) {
     if (this._.state === null) this._.state = defaultState;
     return this._.state;
+  }
+  useMedia(mediaQuery) {
+    const mediaQueryString = Object.entries(mediaQuery).map(([k, v]) => `(${_camelCaseToKebabCase(k)}: ${_addPx(v)})`).join(' and ');
+    let mediaQueryList = _mediaQueryLists[mediaQueryString];
+    if (!mediaQueryList) {
+      mediaQueryList = window.matchMedia(mediaQueryString);
+      _mediaQueryLists[mediaQueryString] = mediaQueryList;
+    }
+    let listener = this._.mediaListeners[mediaQueryString];
+    if (!listener) {
+      listener = () => {
+        this.rerender();
+      };
+      mediaQueryList.addEventListener("change", listener);
+      this._.mediaListeners[mediaQueryString] = listener;
+    }
+    return mediaQueryList.matches;
   }
   rerender() {
     _rerenderNextFrame(this);
@@ -42,65 +57,71 @@ function makeComponent(_render) {
   return (...argsOrProps) => {
     const argCount = Math.max(0, _render.length - 1);
     const args = argsOrProps.slice(0, argCount);
-    const props = argsOrProps[argCount];
-    return new Component(_render, args, props);
+    const props = {...argsOrProps[argCount]};
+    const key = props.key;
+    delete props.key;
+    return new Component(_render, args, key, props);
   }
 }
+function _copyComponent(component) {
+  const newComponent = new Component(component._render, component.args, component.key, component.props);
+  newComponent._ = component._;
+  return newComponent;
+}
+const _mediaQueryLists = {} // Record<string, MediaQueryList>
 class ComponentMetadata {
   constructor() {
     // navigation
-    this.rootComponent = null; // Component
-    this.indexToChild = []; // ComponentMetadata[]
-    this.keyToChild = {} // Record<string, ComponentMetadata>
-    // metadata
-    this.currIndexedChildCount = 0; // number
+    this.prevComponent = null; // Component | null
+    this.root = null; // RootComponentMetadata
+    this.parent = null // ComponentMetadata | null
+    this.keyToChild = {}; // Record<string, ComponentMetadata>
     this.prevIndexedChildCount = null; // number | null
-    this.usedKeys = new Set(); // Set<string>
     // state
-    this.index = 0; // number
     this.state = null; // any
-    this.prevState = null; // any
-    this.prevNode = null; // HTMLElement | null;
+    this.prevState = null; // any | null
+    this.prevNode = null; // HTMLElement | null
+    this.gcFlag = false; // boolean
+    this.mediaListeners = {} // Record<string, () => void>
   }
 }
 class RootComponentMetadata extends ComponentMetadata {
-  constructor() {
+  constructor(component, parentNode) {
     super();
-    this.rerenderList = [];
+    this.root = this
+    this.component = component;
+    this.parentNode = parentNode;
+    this.willRerenderNextFrame = false;
   }
 }
 
 // render
-function renderRoot(rootComponent, parentNode = null) {
-  rootComponent._ = new RootComponentMetadata();
-  rootComponent._.rootComponent = rootComponent;
-  window.onload = () => _renderElements(rootComponent, parentNode ?? document.body);
+function renderRoot(component, parentNode = null) {
+  component._ = new RootComponentMetadata(component, parentNode);
+  window.onload = () => {
+    component._.parentNode = component._.parentNode ?? document.body;
+    _renderElements(component, component._.parentNode);
+  }
 }
 function _renderElements(component, parentNode, isStartNode = true) {
   // render elements
   const {_} = component;
-  _.usedKeys = new Set(); // Set<string>
-  _.currIndexedChildCount = 0; // number
   const node = component._render.bind(component)(...component.args, component.props);
-  // check if indices changed
-  const currIndexedChildCount = _.currIndexedChildCount;
+  // set prev state
+  const indexedChildCount = component._indexedChildCount;
   const prevIndexedChildCount = _.prevIndexedChildCount;
-  if (prevIndexedChildCount === null) {
-    _.prevIndexedChildCount = currIndexedChildCount;
-  } else if (currIndexedChildCount !== prevIndexedChildCount) {
-    console.error(`indexedChildCount changed ${prevIndexedChildCount} -> ${currIndexedChildCount} at`, component._render)
-    throw `Varying children should have a "key" prop.`;
+  if (prevIndexedChildCount !== null && (indexedChildCount !== prevIndexedChildCount)) {
+    console.warn(`Varying children should have a "key" prop. (${prevIndexedChildCount} -> ${indexedChildCount})`, _.prevComponent, component);
   }
-  // filter keyed items
-  const usedKeys = _.usedKeys;
-  _.keyToChild = Object.fromEntries(Object.entries(_.keyToChild).filter(([k, _v]) => usedKeys.has(k)));
+  _.prevIndexedChildCount = indexedChildCount;
   _.prevState = {..._.state};
+  _.prevComponent = component;
   // append elements
   const {style = {}, attribute = {}} = component.props;
   if (node) {
     for (let [k, v] of Object.entries(style)) {
       // TODO: add px to numbers
-      node.style[_camelCaseToKebabCase(k)] = v;
+      node.style[_camelCaseToKebabCase(k)] = _addPx(v);
     }
     for (let [k, v] of Object.entries(attribute)) {
       node.setAttribute(_camelCaseToKebabCase(k), v);
@@ -110,7 +131,7 @@ function _renderElements(component, parentNode, isStartNode = true) {
     }
     const prevNode = _.prevNode;
     if (isStartNode && prevNode) {
-      prevNode.before(node);
+      prevNode.after(node);
       prevNode.remove();
     } else {
       parentNode.append(node);
@@ -126,39 +147,40 @@ function _renderElements(component, parentNode, isStartNode = true) {
 function _camelCaseToKebabCase(k) {
   return k.match(/[A-Z][a-z]*|[a-z]+/g).map(v => v.toLowerCase()).join("-");
 }
+function _addPx(v) {
+  return (v?.constructor?.name === "Number") ? `${v}px` : v;
+}
 
 // rerender
+function _unloadUnusedComponents(prevComponent, gcFlag) {
+  for (let child of prevComponent.children) {
+    const child_ = child._;
+    if (child_.gcFlag !== gcFlag) {
+      for (let [k, listener] of Object.entries(child_.mediaListeners)) {
+        _mediaQueryLists[k].removeEventListener('change', listener);
+      }
+      delete child_.parent.keyToChild[child.key];
+    }
+    _unloadUnusedComponents(child, gcFlag);
+  }
+}
 let _doRerenderNextFrame = false;
-function _rerenderNextFrame(component) {
-  const rootComponent = component._.rootComponent;
-  rootComponent._.rerenderList.push(component);
-  if (!_doRerenderNextFrame) {
+function _rerenderNextFrame(_component) {
+  const rootComponent = _component._.root.component;
+  const {_} = rootComponent;
+  const newGcFlag = !_.gcFlag;
+  if (!rootComponent.willRerenderNextFrame) {
+    rootComponent.willRerenderNextFrame = true;
     requestAnimationFrame(() => {
-      const rerenderList = rootComponent._.rerenderList;
-      walk(rootComponent, (component) => {
-        const shouldRerender = rerenderList.includes(component);
-        if (shouldRerender) {
-          component.children = [];
-          const parentNode = (component === rootComponent) ? document.body : null;
-          _renderElements(component, parentNode);
-        }
-        return shouldRerender;
-      });
-      rootComponent._.rerenderList = [];
-      _doRerenderNextFrame = false;
+      const newRootComponent = _copyComponent(rootComponent);
+      _.gcFlag = newGcFlag;
+      _.component = newRootComponent;
+      _renderElements(newRootComponent, newRootComponent._.parentNode);
+      _unloadUnusedComponents(rootComponent, newGcFlag);
+      rootComponent.willRerenderNextFrame = false;
     });
-    _doRerenderNextFrame = true;
   }
 }
-function walk(component, f) {
-  const shouldBreak = f(component);
-  if (!shouldBreak) {
-    for (let child of component.children) {
-      walk(child, f);
-    };
-  }
-}
-// TODO: use https://developer.mozilla.org/en-US/docs/Web/API/MediaQueryList
 
 // components
 const span = makeComponent(function span(text, _props) {
@@ -170,17 +192,20 @@ const div = makeComponent(function div(_props) {
   return document.createElement('div');
 })
 const input = makeComponent(function input(props) {
+  const { type, value, onInput, onChange } = props;
   const state = this.useState({ needFocus: false });
   const e = this._?.prevNode ?? document.createElement('input');
-  e.type = props.type ?? "text";
-  if (props.onInput) e.oninput = (event) => {
-    props.onInput(event);
+  if (value != null) e.value = value;
+  e.type = type ?? "text";
+  if (onInput) e.oninput = (event) => {
+    onInput(event);
     state.needFocus = true;
   }
-  if (props.onChange) e.onchange = props.onChange;
+  if (onChange) e.onchange = onChange;
   setTimeout(() => {
     if (state.needFocus) {
-      this._.prevNode.focus();
+      //e.setSelectionRange(e.value.length, e.value.length);
+      e.focus();
       state.needFocus = false;
     }
   });
