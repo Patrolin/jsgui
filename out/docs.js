a2254c297f653e55f4f4d11191f7611cb928dece
Patrolin <patrik@balas.cz>
2024-08-26 21:07:52 +0200

    update version string

--- out/docs.js
+++ out/docs.js
@@ -86,7 +86,7 @@ var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
 };
 // TODO: make a typescript compiler to compile packages like odin
 // utils
-var JSGUI_VERSION = "v0.7";
+var JSGUI_VERSION = "v0.8";
 function parseJsonOrNull(jsonString) {
     try {
         return JSON.parse(jsonString);
